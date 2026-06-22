31058cad318054e7aba09f611f8e46cf08a08a0e
ScogganE <scogganE8595@student.faytechcc.edu>
2026-06-21 22:50:38 -0400

    Add files via upload

---
+++ P1LAB1_ScogganEthan.py
@@ -0,0 +1,10 @@
+# Ethan Scoggan
+# 6/21/2026
+# P1LAB1
+# This program prompts the user for their first and last name separately and displays a welcoming message to the course.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
